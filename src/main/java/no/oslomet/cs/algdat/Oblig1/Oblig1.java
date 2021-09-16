0bb12ec2745fe5e0ce67ac705d13ab7f410f1e69
Nourhat <s351892oslomet.no>
2021-09-16 10:58:50 +0200

    tok med arayet som var tom slik det returneres 0

--- src/main/java/no/oslomet/cs/algdat/Oblig1/Oblig1.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig1/Oblig1.java
@@ -100,6 +100,9 @@ mindre
     public static int antallUlikeUsortert(int[] a) {
 
         int antal=a.length;
+        if (a.length < 1) {
+            return 0;
+        }
         for (int i=0;i<a.length;i++){
             for (int j=i+1;j<a.length;j++){
                 if(a[j]==a[i]){
