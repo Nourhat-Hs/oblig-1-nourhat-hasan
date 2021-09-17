6a50c3b4ab281ecc360836d82744b1ca29d14f06
Nourhat <s351892oslomet.no>
2021-09-17 19:06:00 +0200

    oppgave 4 prøver å løse den

--- src/main/java/no/oslomet/cs/algdat/Oblig1/Oblig1.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig1/Oblig1.java
@@ -3,6 +3,7 @@ package no.oslomet.cs.algdat.Oblig1;
 ////// Løsningsforslag Oblig 1 ////////////////////////
 
 import java.lang.UnsupportedOperationException;
+import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 public class Oblig1 {
@@ -121,8 +122,55 @@ mindre
     ///// Oppgave 4 //////////////////////////////////////
     public static void delsortering(int[] a) {
 
-            throw new UnsupportedOperationException();
+          //  throw new UnsupportedOperationException();
+        int antal = 0;
+
+      
+
+
+    }
+    public static void quickSort ( int[] a){   // sorterer hele tabellen
+        kvikksortering0(a, 0, a.length - 1);
+    }
+    public static void kvikksortering ( int[] a, int fra, int til){
+        kvikksortering0(a, fra, til-1);
+    }
+
+
+
+    private static void kvikksortering0(int[] a, int v, int h) {
+        if (v >= h){
+            return;
         }
+        int k = sParter0(a, v, h, (v+h)/2);
+        kvikksortering0(a, v, k-1);
+        kvikksortering0(a, k+1, h);
+    }
+
+    private static int sParter0(int[] a, int v, int h, int indeks) {  /// Kilde: Kombendie/ Programkode 1.3.9 a)
+
+        bytt(a, indeks, h);           // skilleverdi a[indeks] flyttes bakerst
+        int pos = parter0(a, v, h - 1, a[h]);  // partisjonerer a[v:h - 1]
+        bytt(a, pos, h);              // bytter for å få skilleverdien på rett plass
+        return pos;                   // returnerer posisjonen til skilleverdien
+
+    }
+
+    private static int parter0(int[] a, int v, int h, int skilleverdi) /// Kilde: Kombendie
+    {
+        while (true)            // stopper når v > h
+        {
+            while (v <= h && a[v] < skilleverdi) v++;   // h er stoppverdi for v
+            while (v <= h && a[h] >= skilleverdi) h--;  // v er stoppverdi for h
+
+            if (v < h) bytt(a,v++,h--);                 // bytter om a[v] og a[h]
+            else  return v;  // a[v] er nåden første som ikke er mindre enn skilleverdi
+        }
+    }
+
+
+
+
 
 
 
